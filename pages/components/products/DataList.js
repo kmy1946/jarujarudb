d09d0def64614413e756b9522d6a9404368b3411
kmy1946 <kmy1946@outlook.jp>
2022-03-23 15:34:14 +0900

    '12thcommit'

--- pages/components/products/DataList.js
+++ pages/components/products/DataList.js
@@ -1,26 +1,63 @@
 import { Button, Card, CardActions, CardContent, CardMedia, Grid, Typography } from "@mui/material";
+import { useEffect } from "react";
 import styles from './Datalist.module.css';
 
 export default function DataList(props){
-  
+  useEffect(() => {
+    console.log(props.datas.length)
 
+  },[props.datas])
   return (
       <Grid container direction="column">
         <Grid container className={styles.datalist}>
-
-        {() => {
-          if (props.datas) {
-            return (
-              <di>
-                true
-              </di>
-            )
-          } else {
-            <div>
-              NOddata
-            </div>
-          }
-        }}
+        {
+          (
+            props.datas.length == 0 ?
+              <>
+                Sorry No Data...
+              </>
+              :
+              <>
+              {props.datas.map(data => {
+                  const beforestr = data.url
+                  const regex = /(?<=v=)(.*)/
+                  const result = beforestr.match(regex);
+                  const urlv = result[0]
+                  
+                  let gotolink = 'https://www.youtube.com/watch?v='+urlv
+                  const viewstext = ' 回視聴'
+                  
+                  return(
+                    <Grid item xs={12} sm={3} key={data.id}>
+                      <Card className={styles.datalist_card}>
+                        <CardMedia style={{ height: "170px" }} image={data.thumbnail} />
+                        <CardContent>
+                        <Typography variant="body2" component="p" className={styles.datalist_title}>
+                          {data.title}
+                        </Typography>
+                        <Typography variant="body2" component="p">
+                            {data.detail}
+                        </Typography>
+                        
+                        <Typography variant='body2' component="p" className={styles.datalist_created_at}>
+                          {data.created_at}
+                        </Typography>
+                        <Typography variant='body2' component="p" className={styles.datalist_views}>
+                          {data.views}{viewstext}
+                        </Typography>
+                        </CardContent>
+                        <CardActions>
+                          <Button size="small" href={gotolink} target='_blank' className={styles.datalist_gotolink}>動画をみる</Button>
+                        </CardActions>
+                      </Card>
+                    </Grid>
+                  )
+                  }
+                  )
+                  }
+              </>
+          )
+        }
 
         </Grid>
       </Grid>

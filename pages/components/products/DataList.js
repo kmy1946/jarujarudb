aa0d7d4eeac811449eea41c2bb9a844d8b21f5b3
kmy1946 <kmy1946@outlook.jp>
2022-03-23 16:08:30 +0900

    '16thcommit'

--- pages/components/products/DataList.js
+++ pages/components/products/DataList.js
@@ -18,44 +18,42 @@ export default function DataList(props){
           {console.log(datas)}
 
           {datas.length > 0 && (
-                    datas.map(data => (
-                      datas.map(data => {
-                        const beforestr = data.url
-                        const regex = /(?<=v=)(.*)/
-                        const result = beforestr.match(regex);
-                        const urlv = result[0]
-                        
-                        let gotolink = 'https://www.youtube.com/watch?v='+urlv
-                        const viewstext = ' 回視聴'
-                        
-                        return(
-                          <Grid item xs={12} sm={3} key={data.id}>
-                            <Card className={styles.datalist_card}>
-                              <CardMedia style={{ height: "170px" }} image={data.thumbnail} />
-                              <CardContent>
-                              <Typography variant="body2" component="p" className={styles.datalist_title}>
-                                {data.title}
-                              </Typography>
-                              <Typography variant="body2" component="p">
-                                  {data.detail}
-                              </Typography>
-                              
-                              <Typography variant='body2' component="p" className={styles.datalist_created_at}>
-                                {data.created_at}
-                              </Typography>
-                              <Typography variant='body2' component="p" className={styles.datalist_views}>
-                                {data.views}{viewstext}
-                              </Typography>
-                              </CardContent>
-                              <CardActions>
-                                <Button size="small" href={gotolink} target='_blank' className={styles.datalist_gotolink}>動画をみる</Button>
-                              </CardActions>
-                            </Card>
-                          </Grid>
-                        )
-                        }
-                        )
-                    )
+            datas.map(data => {
+              const beforestr = data.url
+              const regex = /(?<=v=)(.*)/
+              const result = beforestr.match(regex);
+              const urlv = result[0]
+              
+              let gotolink = 'https://www.youtube.com/watch?v='+urlv
+              const viewstext = ' 回視聴'
+              
+              return(
+                <Grid item xs={12} sm={3} key={data.id}>
+                  <Card className={styles.datalist_card}>
+                    <CardMedia style={{ height: "170px" }} image={data.thumbnail} />
+                    <CardContent>
+                    <Typography variant="body2" component="p" className={styles.datalist_title}>
+                      {data.title}
+                    </Typography>
+                    <Typography variant="body2" component="p">
+                        {data.detail}
+                    </Typography>
+                    
+                    <Typography variant='body2' component="p" className={styles.datalist_created_at}>
+                      {data.created_at}
+                    </Typography>
+                    <Typography variant='body2' component="p" className={styles.datalist_views}>
+                      {data.views}{viewstext}
+                    </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small" href={gotolink} target='_blank' className={styles.datalist_gotolink}>動画をみる</Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              )
+              }
+              )
           ))}
       
 

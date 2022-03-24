091ec96de6b9fbe344080f1d320d7654a732f1f2
kmy1946 <kmy1946@outlook.jp>
2022-03-24 12:38:11 +0900

    'lib_db_commit'

---
+++ pages/components/AllSearch/AllSearch.js
@@ -0,0 +1,41 @@
+import { Button, TextField } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import { IconButton } from '@mui/material';
+import styles from "./AllSearch.module.css";
+import { useCallback, useState } from 'react';
+
+export default function AllSearch() {
+  const [searchKeyword, setSearchKeyword] = useState("")
+
+    const inputSearchKeyword = useCallback((event) => {
+        setSearchKeyword(event.target.value)
+    }, [setSearchKeyword])
+
+  return (
+    <div>
+      <br/>
+      <br/>
+      <div className={styles.all_search_searchField}>
+          <TextField
+            fullWidth={false} label={"キーワード検索"}
+            multiline={false}
+            size="small"
+            onChange={inputSearchKeyword}
+            value={searchKeyword}
+            required={false} rows={1} 
+            type={"text"}
+            className={styles.all_search_textfield}
+          />
+          <IconButton>    
+        </IconButton>
+      </div>
+      <br/>
+      <div className={styles.all_search_search_button}>
+        <Button className={styles.all_search_search_button_button}>
+          検索
+          <SearchIcon />
+        </Button>
+      </div>
+    </div>
+  )
+}
--- pages/components/layout.js
+++ pages/components/layout.js
@@ -4,6 +4,7 @@ import Header from "./base/header";
 import Heading from "./base/Heading/Heading";
 import TopPageTitle from "./TopPage/TopPageTitle.js/TopPageTitle";
 import Categories from "./base/Categories/Categories";
+import AllSearch from "./AllSearch/AllSearch";
 
 export default function Layout(props) {
   return (
@@ -14,7 +15,10 @@ export default function Layout(props) {
       </Head>
       <Header header={props.header} />
         <Categories/>
+        <AllSearch/>
         <TopPageTitle/>
+        <br/>
+        <br/>
         {props.children}
     </div>
   )

86984c203bd31b5ff711d9849f6af83acfd2b4e0
kmy1946 <kmy1946@outlook.jp>
2022-03-28 15:53:35 +0900

    '20220328'

--- pages/netanotane.js
+++ pages/netanotane.js
@@ -28,6 +28,7 @@ export default function Netanotane() {
   //取得データのセットと総データ件数をセットする
   const setNetanotaneListAPI = async(page) => {
     const tag='ネタのタネ';
+    const orderby = 'ASC'
     const response = await fetch(`https://jarujarudb.vercel.app/api/moviesdb/tag?page=${page}&tag=${tag}`);
     //const response = await fetch(`http://localhost:3000/api/moviesdb/tag?page=${page}&tag=${tag}`)
     const data = await response.json();

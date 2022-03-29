78c5b66ee420e9e2c80d53ae1d6c9d7add3cfd0f
kmy1946 <kmy1946@outlook.jp>
2022-03-30 01:00:09 +0900

    'about_commit'

--- pages/api/movies/page/[id].js
+++ pages/api/movies/page/[id].js
@@ -1,3 +1,4 @@
+/*
 import Link from 'next/link';
 import { Pagination } from '../../../components/Pagination';
 
@@ -61,3 +62,4 @@ export const getStaticProps = async (context) => {
     }
   };
 };
+*/

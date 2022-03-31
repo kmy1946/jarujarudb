957f39f3dd84d7b3c1a7a5ae52f9f19d78c79253
kmy1946 <kmy1946@outlook.jp>
2022-03-31 21:55:09 +0900

    'search_commit'

--- pages/api/moviesdb/moviesdb.js
+++ pages/api/moviesdb/moviesdb.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
   const keyword = 'ジャルジャル';
 
   const ORDER_BY = 'ASC';
-  const PAGE_NUM = 2;//1ページに表示する件数
+  const PAGE_NUM = 16;//1ページに表示する件数
   const offset_coefficient = !req.query || !req.query.page ? 0: req.query.page - 1;//ページ番号
   //count(*) as count
   const query_ = `Select row_number() over() as no, *,
